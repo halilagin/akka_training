44dde2c6fe51107a980585f1365a34b700ce79dc
Halil Ağın <halilagin@Halils-Mac-mini.local>
2021-09-12 21:49:00 +0100

    build script for akka_http done

--- akka_http/scripts/build-docker.js
+++ akka_http/scripts/build-docker.js
@@ -1,30 +1,58 @@
 #!/usr/bin/env zx
 
-fs = require('fs');
-let JARFILE="akka_http.jar"
-let SCALA_VERSION="2.13"
-let MAINCLASS="part3_highlevelhttp.HigLevelHttp"
-let DOCKERTAG="onomoly/akka_http_training:0.0.1"
-let script_path = path.dirname(__filename);
-let projectPath=script_path.split("/").slice(0,-1).join("/");
-let dockerDirPath=`${projectPath}/docker`
-let dockerPath=`${dockerDirPath}/Dockerfile`
-let jarPath=`${projectPath}/target/scala-${SCALA_VERSION}/${JARFILE}`
+var DOCKERTAG="onomoly/akka_http_training:0.0.1";
 
-$`mkdir -p ${dockerDirPath}`
-$`cp ${jarPath} ${dockerDirPath}`
+function build(args) {
+    fs = require('fs');
+    let JARFILE="akka_http.jar";
+    let SCALA_VERSION="2.13";
+    let MAINCLASS="part3_highlevelhttp.HigLevelHttp";
+    let script_path = path.dirname(__filename);
+    let projectPath=script_path.split("/").slice(0,-1).join("/");
+    let dockerDirPath=`${projectPath}/docker`;
+    let dockerPath=`${dockerDirPath}/Dockerfile`;
+    let jarPath=`${projectPath}/target/scala-${SCALA_VERSION}/${JARFILE}`;
 
-let dockerfileContent=`
-FROM openjdk:8
-RUN mkdir -p /app
-WORKDIR /app
-COPY ${JARFILE} . /app
-EXPOSE 8080
-CMD ["java", "-cp", "/app/${JARFILE}", "${MAINCLASS}"]
-`
+    $`mkdir -p ${dockerDirPath}`;
+    $`cp ${jarPath} ${dockerDirPath}`;
 
-fs.writeFile(dockerPath, dockerfileContent, function (err) {
-  if (err) return console.log(err);
-});
+    let dockerfileContent=`
+    FROM openjdk:8
+    RUN mkdir -p /app
+    WORKDIR /app
+    COPY ${JARFILE} . /app
+    EXPOSE 8080
+    CMD ["java", "-cp", "/app/${JARFILE}", "${MAINCLASS}"]
+    `;
 
-$`cd ${dockerDirPath} && docker build -t ${DOCKERTAG} . `
+    fs.writeFile(dockerPath, dockerfileContent, function (err) {
+      if (err) return console.log(err);
+    });
+
+    $`cd ${dockerDirPath} && docker build -t ${DOCKERTAG} . `;
+}
+
+
+function run_docker(args){
+    let container_name=args[0];
+    if (container_name==undefined || container_name==null || container_name.trim()==""){
+        console.log(`usage: zx ${process.argv[2]} run_docker <docker_container_name> `);
+        return;
+    }
+    $`docker run --rm -p 8080:8080 --name ${container_name} ${DOCKERTAG}`
+}
+
+
+if (process.argv.length<4){
+    console.log(`usage: zx ${process.argv[2]} [build|run_docker|kube_deploy] `);
+    return;
+} 
+
+command=process.argv[3];
+args=process.argv.slice(4);
+if (command=="build")
+    build(args);
+if (command=="run_docker")
+    run_docker(args);
+
+
